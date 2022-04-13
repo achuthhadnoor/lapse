720ed32c2dd23fcf5df2b9f759e278e2ea46d125
achuthhadnoor <armorachyuth123@gmail.com>
2022-04-13 06:27:22 +0530

    initial app setup

---
+++ src/renderer/App.tsx
@@ -0,0 +1,19 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+const Hello = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
